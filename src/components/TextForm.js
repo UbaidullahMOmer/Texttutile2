ecef9dd4fa7d1a546b74cf9e6d01a7a0f29cb01f
I-want-place-in-your-heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-01-11 12:13:24 +0500

    uppercase
    Uppercase feature completed.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleOnChange = (event)=>{
+        console.log("OnChange");
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('Entre the text here');
     return (
         <div>
             <h1>{props.headding}</h1>
             <div className="mb-3">   
-                <textarea  className="form-control" id="mybox" rows="10"></textarea>
+                <textarea  className="form-control" onChange={handleOnChange} value={text} id="mybox" rows="10"></textarea>
             </div>
-            <button className='btn btn-primary'>Convert to Uppercase</button>
+            <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
     )
 }
