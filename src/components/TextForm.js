e4f44ebc30341b96b499213feb0c0d459d3d8528
I-want-place-in-your-heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-01-11 12:20:11 +0500

    small change
    Placeholder add  in text area

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,7 +15,7 @@ export default function TextForm(props) {
         <div>
             <h1>{props.headding}</h1>
             <div className="mb-3">   
-                <textarea  className="form-control" onChange={handleOnChange} value={text} id="mybox" rows="10"></textarea>
+                <textarea  className="form-control" onChange={handleOnChange} value={text} placeholder="Type here" id="mybox" rows="10"></textarea>
             </div>
             <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
