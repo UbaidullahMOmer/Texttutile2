249e2ae99d3378b7892f7cc4a1515a8e1ca7c987
Heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-01-13 09:25:54 -0800

    Revert "some more add"
    This reverts commit 292bf602b39b2501616bd86da9878c56553efbac.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
-import BasicTable from "./components/BasicTable"
 function App() {
   return (
     <>
       <Navbar title="Texttutile" about="About" />
       <div className="container my-3">
       <TextForm headding="Enter the text to analyze below"/>
-      <BasicTable/>
       </div>
     </>
   )
