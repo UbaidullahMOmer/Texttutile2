f72caabf883427c917867163d8988a3b6fd36b12
Heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-01-29 22:46:14 -0800

    akd

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,7 +16,7 @@ export default function TextForm(props) {
             <div className='container'>
                 <h1>{props.headding}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={handleOnChange} value={text} placeholder="Type here" id="mybox" rows="10"></textarea>
+                    <textarea  className="form-control" onChange={handleOnChange} value={text} placeholder="Type here" id="mybox" rows="10"></textarea>
                 </div>
                 <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
             </div>
