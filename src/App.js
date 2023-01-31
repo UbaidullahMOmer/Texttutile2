a6bbba7feb1002e32c7211430192d3ea5cda4331
Heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-01-31 06:51:33 -0800

    add country

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import axios from "axios"
 
 function App() {
   const [news, setNews] = useState([])
-  const [con, setCon] = useState("health")
+  const [con, setCon] = useState("general")
+  const [ca, setCa] = useState("us")
 
 
-  useEffect(() => {axios.get(`https://newsapi.org/v2/top-headlines?category=${con}&apiKey=6343d39ae0724883bb56efd258a98abc`)
+  useEffect(() => {axios.get(`https://newsapi.org/v2/top-headlines?country=${ca}&category=${con}&apiKey=6343d39ae0724883bb56efd258a98abc`)
       .then((res) => {
         console.log(res.data.articles);
         setNews(res.data.articles)
@@ -25,27 +26,35 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="!">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="!" onClick={() => { setCon("in") }}>India</a>
+                <a className="nav-link active" onClick={() => { setCa("us") }}  class="btn btn-outline-primary" aria-current="page" href="!">Home</a>
               </li>
               <div className="btn-group">
                 <button type="button" className="btn  dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                   Country
                 </button>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="!" onClick={() => { setCon("health") }}>Action</a></li>
+              
+                  <ul className="dropdown-menu" aria-labelledby="dropdownMenu btn btn-outline-primary">
+                  <li onClick={() => { setCa("us") }} class="dropdown-item" >United kigdom</li>
+                  <li onClick={() => { setCa("hk") }} class="dropdown-item" >Hong Kong</li>
+                  <li onClick={() => { setCa("au") }} class="dropdown-item" >Australia</li>
+                  <li onClick={() => { setCa("za") }} class="dropdown-item" >South Africa</li>
+                  <li onClick={() => { setCa("gr") }} class="dropdown-item" >Greece</li>
+                  <li onClick={() => { setCa("na") }} class="dropdown-item" >New Zealand</li>
+                  <li onClick={() => { setCa("kr") }} class="dropdown-item" >South Korea</li>
+                  <li onClick={() => { setCa("at") }} class="dropdown-item" >Austria</li>
+                  <li onClick={() => { setCa("hu") }} class="dropdown-item" >Hungary</li>
+                  <li onClick={() => { setCa("gr") }} class="dropdown-item" >Greece</li>
                 </ul>
+             
               </div>
               <div className="btn-group">
                 <button type="button" className="btn  dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                   Category
                 </button>
-                <ul className="dropdown-menu">
-                  <li onClick={() => { setCon("health") }}>Action</li>
-                  <li onClick={() => { setCon("gr") }}>Greece</li>
-
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li onClick={() => { setCon("health") }}  class="dropdown-item">Health</li>
+                  <li onClick={() => { setCon("science") }} class="dropdown-item">Science</li>
+                  <li onClick={() => { setCon("sports") }}  class="dropdown-item">Sport</li>
                 </ul>
               </div>
             </ul>
