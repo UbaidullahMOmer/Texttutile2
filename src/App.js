a04b594f8bc8786432f79e89536c724896f98e1d
Heart <121765405+I-want-place-in-your-heart@users.noreply.github.com>
2023-02-01 04:52:40 -0800

    take show

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   const [news, setNews] = useState([])
   const [con, setCon] = useState("general")
   const [ca, setCa] = useState("in")
-  const [cah, setCah] = useState("Gernal")
+  const [cah, setCah] = useState("India")
   const [coh, setCoh] = useState("Gernal")
 
 
 
-  useEffect(() => {axios.get(`https://newsapi.org/v2/top-headlines?country=${ca}&category=${con}&apiKey=75fa6547b40c4f989a8758a77fd15552`)
+  useEffect(() => {axios.get(`https://newsapi.org/v2/top-headlines?country=${ca}&category=${con}&apiKey=38daea198b1140c1b400c32ab3f20a2d`)
       .then((res) => {
         console.log(res.data.articles);
         setNews(res.data.articles)
@@ -40,15 +40,15 @@ function App() {
               
                   <ul className="dropdown-menu" aria-labelledby="dropdownMenu btn btn-outline-primary">
                   <li  onClick={() => { setCa("us"); setCah("United kigdom");} } class="dropdown-item" >United kigdom</li>
-                  <li onClick={() => { setCa("hk") }} class="dropdown-item" >Hong Kong</li>
-                  <li onClick={() => { setCa("au") }} class="dropdown-item" >Australia</li>
-                  <li onClick={() => { setCa("za") }} class="dropdown-item" >South Africa</li>
-                  <li onClick={() => { setCa("gr") }} class="dropdown-item" >Greece</li>
-                  <li onClick={() => { setCa("na") }} class="dropdown-item" >New Zealand</li>
-                  <li onClick={() => { setCa("kr") }} class="dropdown-item" >South Korea</li>
-                  <li onClick={() => { setCa("at") }} class="dropdown-item" >Austria</li>
-                  <li onClick={() => { setCa("hu") }} class="dropdown-item" >Hungary</li>
-                  <li onClick={() => { setCa("gr") }} class="dropdown-item" >Greece</li>
+                  <li onClick={() => { setCa("hk"); setCah("Hong Kong"); }} class="dropdown-item" >Hong Kong</li>
+                  <li onClick={() => { setCa("au"); setCah("Australia"); }} class="dropdown-item" >Australia</li>
+                  <li onClick={() => { setCa("za"); setCah("South Africa"); }} class="dropdown-item" >South Africa</li>
+                  <li onClick={() => { setCa("gr"); setCah("Greece"); }} class="dropdown-item" >Greece</li>
+                  <li onClick={() => { setCa("na"); setCah("New Zealand"); }} class="dropdown-item" >New Zealand</li>
+                  <li onClick={() => { setCa("kr"); setCah("South Korea"); }} class="dropdown-item" >South Korea</li>
+                  <li onClick={() => { setCa("at"); setCah("Austria"); }} class="dropdown-item" >Austria</li>
+                  <li onClick={() => { setCa("hu"); setCah("Hungary"); }} class="dropdown-item" >Hungary</li>
+                  <li onClick={() => { setCa("gr"); setCah("Greece"); }} class="dropdown-item" >Greece</li>
                 </ul>
              
               </div>
@@ -57,13 +57,13 @@ function App() {
                   Category
                 </button>
                 <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                  <li onClick={() => { setCon("health"); setCoh("Health") }}  class="dropdown-item">Health</li>
-                  <li onClick={() => { setCon("science") }} class="dropdown-item">Science</li>
-                  <li onClick={() => { setCon("sports") }}  class="dropdown-item">Sport</li>
-                  <li onClick={() => { setCon("technology") }}  class="dropdown-item">technology</li>
-                  <li onClick={() => { setCon("entertainment") }}  class="dropdown-item">entertainment</li>
-                  <li onClick={() => { setCon("business") }}  class="dropdown-item">business</li>
-                  <li onClick={() => { setCon("general") }}  class="dropdown-item">general</li>
+                  <li onClick={() => { setCon("health"); setCoh("Health"); }}  class="dropdown-item">Health</li>
+                  <li onClick={() => { setCon("science"); setCoh("Science"); }} class="dropdown-item">Science</li>
+                  <li onClick={() => { setCon("sports"); setCoh("Sport"); }}  class="dropdown-item">Sport</li>
+                  <li onClick={() => { setCon("technology"); setCoh("Technology"); }}  class="dropdown-item">Technology</li>
+                  <li onClick={() => { setCon("entertainment"); setCoh("Entertainment"); }}  class="dropdown-item">Entertainment</li>
+                  <li onClick={() => { setCon("business"); setCoh("Business"); }}  class="dropdown-item">Business</li>
+                  <li onClick={() => { setCon("general"); setCoh("General"); }}  class="dropdown-item">General</li>
                 </ul>
               </div>
             </ul>
@@ -76,16 +76,18 @@ function App() {
       </nav>
       <h1 className=".m-5" style={{ 
             marginTop : "30px"
-            ,marginLeft : "200px"
-           }}>Country : {cah} <br></br>
-              Category : {coh}</h1>
+            ,marginLeft : "200px",
+            color: "#0b2d39"
+           }}>Latest News <br></br>
+            Country : <span style={{color:"#1a6985" , fontSize:"36px"}}>{cah}</span>  <br></br>
+              Category : <span style={{color:"#1a6985" ,fontSize:"36px"}}>{coh}</span></h1>
       <div className="container my-5">
         <div className="row text-center">
           {
             news.map((val) => {
               return (
                 <div className="col my-3">
-                  <div className="card" style={{ width: "30rem" }}>
+                  <div className="cards" style={{ width: "30rem" }}>
                     <img src={val.urlToImage} className="card-img-top" alt="..." />
                     <div className="card-body">
                       <h5 className="card-title">{val.title}</h5>
